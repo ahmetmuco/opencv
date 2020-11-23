9883a5bec71f6077edb52eb6dac8450b01f62556
Ahmet Mücahit Tarakçı <59475957+ahmetmuco@users.noreply.github.com>
2020-11-23 12:21:01 +0300

    trying something.

--- 4_augmentasyon/4_imgaug.py
+++ 4_augmentasyon/4_imgaug.py
@@ -8,39 +8,20 @@ import numpy as np
 @author = 'Ahmet Mucahit Tarakci'
 """
 
-path = './resimler/saü-logo.PNG'
-img = imageio.imread(path)
+path = 'C:\\Users\\Ahmet\\Desktop\\opencv\\4_augmentasyon\\buyuk_sigorta\\001000014.jpg'
 
-# # Orijinal resim:
-# ia.imshow(img)
-#
-# # -45 veya 45 derece döndürme:
-# rotate = iaa.Affine(rotate=(-45, 45))
-# image_aug = rotate(image=img)
-#
-# # Augmented:
-# ia.imshow(image_aug)
+resim = imageio.imread(path)
+resimler = [resim, resim, resim, resim]
 
-
-# # Birden fazla resim üzerinde işlem yapmak:
-images = [img, img, img, img]
-#
-# # -45 veya 45 derece döndürme:
-# rotate = iaa.Affine(rotate=(-45, 45))
-# images_aug = rotate(images=images)
-#
-# # Zenginleştirilmiş Görüntüler:
-# ia.imshow(np.hstack(images_aug))
-
-
-# Eşzamanlı Olarak Birden Fazla Zenginleştirme Tekniği:
+# Eşzamanlı Olarak Birden Fazla Çoğaltma Tekniği:
 seq = iaa.Sequential([
     iaa.Affine(rotate=(-45, 45)),
     iaa.AdditiveGaussianNoise(scale=(10, 60)),
     iaa.Crop(percent=(0, 0.2))
 ])
-# Zenginleştirme çağrısı:
-images_aug = seq(images=images)
 
-# Zenginleştirilmiş esimler:
-ia.imshow(np.hstack(images_aug))
+# Veri çoğaltma çağrısı:
+augmentasyon_islemi = seq(images=resimler)
+
+# Çoğaltılmış resimler:
+ia.imshow(np.hstack(augmentasyon_islemi))
